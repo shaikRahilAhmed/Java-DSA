144e8ee8fc30ddd7822f9aefadbaccc38da25e29
Shaik Rahil <rahil.ahmed.sk@gmail.com>
2026-01-07 22:47:04 +0530

    day 1 / 7th jan 2026

--- Main.java
+++ Main.java
@@ -41,16 +41,75 @@
 
 
 
-public class Main{
-    public static int mull(int a, int b){
-        int mull = a*b;
-        return mull;
-    }
-    public static void main(String args[]){
-        int a = 2;
-        int b = 3;
-        int prod = mull(a,b);
+// public class Main{
+//     public static int mull(int a, int b){
+//         int mull = a*b;
+//         return mull;
+//     }
+//     public static void main(String args[]){
+//         int a = 2;
+//         int b = 3;
+//         int prod = mull(a,b);
 
-        System.out.println("a*b="+prod);
-    }
-}
+//         System.out.println("a*b="+prod);
+//     }
+// }
+
+
+
+
+// public class Main{
+//     public static int fac (int n){
+//         int f = 1;
+//         for (int i = 1; i<=n; i++){
+//             f=f*i;
+//         }
+//         return f;
+//     }
+//     public static void main(String args[]){
+//         System.out.println(fac(4));
+//     }
+// }
+
+
+
+// public class Main{
+//     public static int fac(int n){
+//         int f = 1;
+//         for(int i =1; i<=n; i++){
+//             f=f*i;
+//         }
+//         return f;
+//     }
+//     public static int binCoff(int n, int r){
+//         int fac_n = fac(n);
+//         int fac_r = fac(r);
+//         int fac_nmr = fac(n-r);
+
+//         int binCoff = fac_n / (fac_r *fac_nmr);
+
+//         return binCoff;
+//     }
+//     public static void main(String args[]){
+//         System.out.println(binCoff(5,2));
+//     }
+// }
+
+
+
+// public class Main{
+//     public static int sum(int a , int b){
+//         return a+b;
+//     }
+//     public static int sum(int a, int b, int c){
+//         return a+b+c;
+//     }
+//     public static void main(String args[]){
+//         System.out.println(sum(2,3));
+//         System.out.println(sum(2,3,6));
+//     }
+// }
+
+
+
+
