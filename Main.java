21269b8b1294818737b09fc24cd534cc3c639734
Shaik Rahil <rahil.ahmed.sk@gmail.com>
2026-01-09 22:36:17 +0530

    day 3 / 9th jan 2026

--- Main.java
+++ Main.java
@@ -359,29 +359,29 @@
 
 
 
-import java.util.*;
-public class Main{
-    public static void subArray(int numbers[]){
-        int totalSubArrays = 0;
-        for(int i = 0; i < numbers.length; i++){
-            int start = i;
-            for(int j = i; j < numbers.length; j++){
-                int end = j;{
-                    for(int k = start; k<=end; k++){
-                        System.out.print(numbers[k]+ " ");
-                    }
-                    totalSubArrays++;
-                    System.out.println();
-                }
-            }System.out.println();
-        }System.out.println("total SubArrays= "+ totalSubArrays);
+// import java.util.*;
+// public class Main{
+//     public static void subArray(int numbers[]){
+//         int totalSubArrays = 0;
+//         for(int i = 0; i < numbers.length; i++){
+//             int start = i;
+//             for(int j = i; j < numbers.length; j++){
+//                 int end = j;{
+//                     for(int k = start; k<=end; k++){
+//                         System.out.print(numbers[k]+ " ");
+//                     }
+//                     totalSubArrays++;
+//                     System.out.println();
+//                 }
+//             }System.out.println();
+//         }System.out.println("total SubArrays= "+ totalSubArrays);
         
-    }
-    public static void main(String args[]){
-        int numbers[] = {2,3,4,5,6};
-        subArray(numbers);
+//     }
+//     public static void main(String args[]){
+//         int numbers[] = {2,3,4,5,6};
+//         subArray(numbers);
     
-    }
-}
+//     }
+// }
 
 
