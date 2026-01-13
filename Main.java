165fa4ae99cf22d943ef49ff1c73a9e35f5ef70f
Shaik Rahil <rahil.ahmed.sk@gmail.com>
2026-01-13 23:12:47 +0530

    day 6 / 13th jan 2026

--- Main.java
+++ Main.java
@@ -419,3 +419,25 @@
 //     }
 // }
 
+
+
+
+
+
+
+// import java.util.*;
+// public class Main{
+//     public static void subarray(int numbers[]){
+//         for(int i = 0; i < numbers.length; i++){
+//             int start = i;
+//         for(int j =i; j<numbers.length; j++){
+//             int end = j;
+//     for(int k = start; k<end; k++){
+
+//     }}}
+// }
+
+//     public static void main(String args[]){
+//         int numbers = {2,3,4,5,6};
+//     }
+// }
