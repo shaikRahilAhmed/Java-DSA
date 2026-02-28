009130f9704ed6d641f0a0afcff9b0c5ccdc9f07
Shaik Rahil <rahil.ahmed.sk@gmail.com>
2026-02-28 20:27:03 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -420,4 +420,40 @@
 // }
 
 
+// public class Main{
+//     public static void rev(int arr[]){
+//         int start = 0;
+//         int end = arr.length-1;
 
+//         while(start < end){
+//             int temp = arr[start];
+//             arr[start] = arr[end];
+//             arr[end] = temp;
+
+//             start++;
+//             end--;
+//         }
+//         for (int i = 0; i<arr.length; i++){
+//             System.out.print(arr[i]+ " ");
+//         }
+//     }
+//     public static void main(String args[]){
+//         int arr[] = {1,2,3,4,5};
+//         rev(arr);
+//     }
+// }
+
+
+public class Main{
+    public static int fact(int n){
+        int fac = 1;
+        for(int i = 1; i <=n; i++){
+            fac = fac*i;
+        }
+        return fac;
+    }
+    public static void main(String args[]){
+        System.out.println(fact(5));
+    }
+    
+}
